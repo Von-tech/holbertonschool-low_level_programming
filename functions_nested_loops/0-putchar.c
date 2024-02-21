09a72401e11966a06daac29a106c629e02dde50b
Vonzell Carson <vonzell@LAPTOP-109H1J4B>
2024-02-21 13:54:07 -0400

    Finale fix on 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,23 @@
 #include <unistd.h>
-
+#include "main.h"
+/**
+ * main - Write a program that prints Holberton, followed by a new*
+ *
+ *
+ * Return: On success 0.
+ */
 int main(void)
 {
-	char str[] = "_putchar\n";
-	write(1, str, sizeof(str) - 1);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	return 0;
-
+	return (0);
 }
 
