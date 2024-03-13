046d2068ea3f47d524250c0059b92af09b5bfcc3
Vonzell Carson <vonzell@LAPTOP-109H1J4B>
2024-03-13 14:57:09 -0400

    fixed code

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,17 +2,14 @@
 #include <stdio.h>
 
 /**
- * print_name - Write a function that prints a name.
- *
- * @name: char pointer
- *
+ * print_name - print name
+ * @name: name to print
  * @f: pointer
  *
- * Return: void
+ * Return: nothing
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
-	f(name);
+	if (f != NULL)
+		f(name);
 }
