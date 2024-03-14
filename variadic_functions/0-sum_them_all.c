d3c9dce1e1e6dcda77c756ee4e3f8d10536fa42f
Vonzell Carson <vonzell@LAPTOP-109H1J4B>
2024-03-14 12:44:55 -0400

    fixing file

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,25 +1,22 @@
-#include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
-
 /**
- * sum_them_all - Returns the sum of all its parameters
- * @n: The number of parameters
- *
- * Return: The sum of all parameters
+ * sum_them_all - adds all args
+ * @n: num of args
+ * Return: result
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list args;
+	va_list valist;
+	unsigned int sum;
 	unsigned int i;
-	int sum = 0;
 
+	sum = 0;
+	va_start(valist, n);
 	if (n == 0)
 		return (0);
-
-	va_start(args, n);
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, int);
-	va_end(args);
-
+		sum += va_arg(valist, unsigned int);
+	va_end(valist);
 	return (sum);
 }
