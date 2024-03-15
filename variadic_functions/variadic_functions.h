4a05678a54f6a86090980617f3a312b9ecacde86
Vonzell Carson <vonzell@LAPTOP-109H1J4B>
2024-03-14 21:51:15 -0400

    Struct Types doc

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,10 +8,17 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct types - A structure representing a type and its corresponding function
+ *
+ * @t: The type identifier
+ * @f: The function pointer to handle the type
+ */
 typedef struct types
 {
 	char t;
 	void (*f)(va_list);
-}
-types;
+} types;
+
 #endif
