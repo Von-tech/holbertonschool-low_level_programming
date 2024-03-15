4e91346a6e201a47af279b4d387a72aecbf084e1
Vonzell Carson <vonzell@LAPTOP-109H1J4B>
2024-03-15 11:33:32 -0400

    Made changes to dog.h file. Added new type dog_t as a new name for the type struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,6 +13,10 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/* Define a new type dog_t as a new name for the type struct dog */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
